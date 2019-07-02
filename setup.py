111d9231c2fa618234b7cd3cb4057ace9fc18b66
QFSW <yusuf.ismail1024@gmail.com>
2019-07-02 20:19:14 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyanoboard',
+      version='0.1.0',
+      description='Simple piano to keyboard adaptor. Works with any MIDI enabled device.',
+      url='https://github.com/QFSW/Pyanoboard',
+      author='QFSW',
+      author_email='support@qfsw.co.uk',
+      license='MIT',
+      packages=['pyanoboard'],
+      zip_safe=False)
