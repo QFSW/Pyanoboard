915322ba74fb0fad5add3ba0ddd669eae50bd3ab
QFSW <yusuf.ismail1024@gmail.com>
2019-07-03 00:58:29 +0100

    Split pyanoboard up into more files

--- pyanoboard/pyanoboard.py
+++ pyanoboard/pyanoboard.py
@@ -1,63 +1,15 @@
 import sys
-import os
-import keys
 import emulator
-import pygame
-import json
-from pygame import midi
-
-
-def print_device_info():
-    for i in range(pygame.midi.get_count()):
-        info = pygame.midi.get_device_info(i)
-        (interface, name, input_, output, open) = info
-
-        if input_:
-            io_status = "input"
-        if output:
-            io_status = "output"
-
-        print("%2i: %s: %s (%s)" %
-              (i, interface, name, io_status))
-
-
-def parse_key(key_str):
-    upper_str = key_str.upper()
-    upper_concat1 = "KEY_" + upper_str
-    upper_concat2 = "VK_" + upper_str
-
-    for attempt in [upper_str, upper_concat1, upper_concat2]:
-        result = getattr(keys, attempt, None)
-        if result is not None and isinstance(result, int):
-            return result
-
-    return int(key_str)
-
-
-def load_config(config_path):
-    file = open(config_path, "r")
-    config = json.load(file)
-
-    device_id = config["device_id"]
-    raw_bindings = config["bindings"]
-
-    bindings = []
-    for raw_binding in raw_bindings:
-        note = raw_binding["note"]
-        raw_key = raw_binding["key"]
-        key = parse_key(raw_key)
-        bindings.append(emulator.Binding(note, key))
-
-    return device_id, bindings
+import debug
+import config
 
 
 def main():
     emulator.init_pygame()
-
     args = sys.argv[1:]
 
     if "-devices" in args:
-        print_device_info()
+        debug.print_device_info()
         sys.exit()
 
     verbose = "-verbose" in args
@@ -67,7 +19,7 @@ def main():
         sys.exit(-1)
     config_path = args[-1]
 
-    (device_id, mappings) = load_config(config_path)
+    (device_id, mappings) = config.load(config_path)
     em = emulator.Emulator(device_id, mappings)
     em.verbose = verbose
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 
 setup(name='pyanoboard',
-      version='1.0.1',
+      version='1.0.3',
       description='Pyanoboard is a simple piano to keyboard emulator, allowing you to use any MIDI enabled device as a virtual keyboard.',
       long_description=long_description,
       long_description_content_type='text/markdown',
