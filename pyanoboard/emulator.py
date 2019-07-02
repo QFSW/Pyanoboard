60a9b5a80cdd721805fbca52db449114e50a3b41
QFSW <yusuf.ismail1024@gmail.com>
2019-07-02 17:41:16 +0100

    Made emulator class

--- pyanoboard/emulator.py
+++ pyanoboard/emulator.py
@@ -1,16 +1,14 @@
-import pygame, os, midi, KeyHandling, time
+import pygame, os, midi
+import midi
 import key_handling, keys
 from pygame import midi
 from pygame.locals import *
 
-os.environ["SDL_VIDEODRIVER"] = "dummy"
 
-MainDir = os.path.split(os.path.abspath(__file__))[0]
-pygame.init()
-pygame.midi.init()
-
-pygame.display.set_mode((1,1))
-Piano = pygame.midi.Input(1)
+def init_pygame():
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
+    pygame.init()
+    pygame.midi.init()
 
 
 class Binding:
@@ -24,44 +22,56 @@ class Binding:
         key_input = key_handling.new_keyboard_input(self.key)
         key_handling.send_input(key_input)
 
-    def un_press(self):
+    def unpress(self):
         self.is_pressed = False
         key_input = key_handling.new_keyboard_input(self.key, keys.KEYEVENTF_KEYUP)
         key_handling.send_input(key_input)
 
 
-NoteMaps = [
-    Binding("C2", KeyHandling.KEY_S),
-    Binding("C#2", KeyHandling.KEY_W),
-    Binding("B1", KeyHandling.KEY_A),
-    Binding("D2", KeyHandling.KEY_D),
-    Binding("C3", KeyHandling.KEY_G),
-    Binding("C#3", KeyHandling.KEY_T),
-    Binding("B2", KeyHandling.KEY_F),
-    Binding("D3", KeyHandling.KEY_H),
-    Binding("C4", KeyHandling.KEY_L),
-    Binding("C#4", KeyHandling.KEY_O),
-    Binding("B3", KeyHandling.KEY_K),
-    Binding("D4", KeyHandling.KEY_R),
-    Binding("D#4", KeyHandling.KEY_P),
-    Binding("F4", KeyHandling.KEY_Z)
-]
+class Emulator:
+    def __init__(self, midi_device_id, bindings):
+        self.midi_device_id = midi_device_id
+        self.midi_device = pygame.midi.Input(midi_device_id)
+        self.bindings = bindings
 
-while True:
-    if Piano.poll():
-        MIDIEvents = Piano.read(10)
-        if MIDIEvents[0][0][1]!=0:
-            for Event in MIDIEvents:
-                if Event[0][0] == 176:
-                    if MIDI.MIDI.ReturnPedal(Event[0])[1] == True:
-                        for Key in NoteMaps:
-                            Key.UnPress()
-                elif Event[0][0] == 144:
-                    Note = MIDI.MIDI.ReturnNote(Event[0])
-                    for Key in NoteMaps:
-                        if Key.Note == Note:
-                            if (MIDI.MIDI.ReturnNoteDynamic(Event[0]) == "OFF"):
-                                Key.UnPress()
-                            else:
-                                Key.Press()
+    def unpress_all(self):
+        for binding in self.bindings:
+            binding.un_press()
+
+    def run(self):
+        while True:
+            if self.midi_device.poll():
+                MIDIEvents = self.midi_device.read(10)
+                if MIDIEvents[0][0][1] != 0:
+                    for Event in MIDIEvents:
+                        if Event[0][0] == 176:
+                            if MIDI.MIDI.ReturnPedal(Event[0])[1] == True:
+                                for Key in NoteMaps:
+                                    Key.UnPress()
+                        elif Event[0][0] == 144:
+                            Note = MIDI.MIDI.ReturnNote(Event[0])
+                            for Key in NoteMaps:
+                                if Key.Note == Note:
+                                    if (MIDI.MIDI.ReturnNoteDynamic(Event[0]) == "OFF"):
+                                        Key.UnPress()
+                                    else:
+                                        Key.Press()
+
+
+NoteMaps = [
+    Binding("C2", keys.KEY_S),
+    Binding("C#2", keys.KEY_W),
+    Binding("B1", keys.KEY_A),
+    Binding("D2", keys.KEY_D),
+    Binding("C3", keys.KEY_G),
+    Binding("C#3", keys.KEY_T),
+    Binding("B2", keys.KEY_F),
+    Binding("D3", keys.KEY_H),
+    Binding("C4", keys.KEY_L),
+    Binding("C#4", keys.KEY_O),
+    Binding("B3", keys.KEY_K),
+    Binding("D4", keys.KEY_R),
+    Binding("D#4", keys.KEY_P),
+    Binding("F4", keys.KEY_Z)
+]
                     
