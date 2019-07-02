8777e9bed64a9c6cbf5587aacfdaa1876df97c30
QFSW <yusuf.ismail1024@gmail.com>
2019-07-02 21:08:18 +0100

    Package now uses readme

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Pyanoboard
 _Pyanoboard_ is a simple piano to keyboard emulator. It allows you to use any MIDI enabled device as a virtual keyboard.
 
+Pyanoboard actually simulates key events rather than sending characters to be typed, which means you can use it for anything such as gaming and not just typing.
+
 ### Prerequisites
 
 - MIDI enabled device such as a digital piano
 - MIDI to USB cable
 - `python` and `pip`
 - Windows
+
--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(name='pyanoboard',
       version='0.1.1',
       description='Simple piano to keyboard adaptor. Works with any MIDI enabled device.',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
       url='https://github.com/QFSW/Pyanoboard',
       author='QFSW',
       author_email='support@qfsw.co.uk',
