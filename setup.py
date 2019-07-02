ba108c3b930ec3f5791437588690d18a922d5a99
QFSW <yusuf.ismail1024@gmail.com>
2019-07-03 00:29:53 +0100

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
 
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(name='pyanoboard',
-      version='1.0.0',
+      version='1.0.1',
       description='Simple piano to keyboard adaptor. Works with any MIDI enabled device.',
-      long_description=readme(),
+      long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/QFSW/Pyanoboard',
       author='QFSW',
